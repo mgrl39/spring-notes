c5804845f98da35a17d845e5cf8bafa69025ea4d
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-05-13 10:27:35 +0200

    restcontroller vs controller code base

---
+++ springboot-restcontroller-vs-controller/src/main/java/net/xeill/elpuig/springbootrestcontrollervscontroller/SpringbootRestcontrollerVsControllerApplication.java
@@ -0,0 +1,13 @@
+package net.xeill.elpuig.springbootrestcontrollervscontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestcontrollerVsControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRestcontrollerVsControllerApplication.class, args);
+    }
+
+}
---
+++ springboot-restcontroller-vs-controller/src/test/java/net/xeill/elpuig/springbootrestcontrollervscontroller/SpringbootRestcontrollerVsControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.xeill.elpuig.springbootrestcontrollervscontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestcontrollerVsControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
